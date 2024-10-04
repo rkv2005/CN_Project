a85a54fd7abfefb47cda20d6e44ee1f216f8515c
rkv_2005_ <raghavkishore2005@gmail.com>
2024-10-04 08:53:57 +0530

    Add initial project files and configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a basic React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
